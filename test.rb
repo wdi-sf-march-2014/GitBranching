58ee7d3fd013b3017a1e3161f06b68dc8b70762e
shane <shanemiller500@gmail.com>
2014-05-22 14:34:56 -0700

    updated

--- test.rb
+++ test.rb
@@ -2,9 +2,9 @@ puts " you had me at Hello World!"
 puts "#yoloswag"
 puts "Hello"
 puts "whats going on?"
-<<<<<<< HEAD
+
 puts "what ever fool..."
-=======
+
 puts "hello shane!!!!"
 puts 'whats uuuuuù33'
->>>>>>> new_branch2
+
